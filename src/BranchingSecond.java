3a596d992294d4be357ba82652f34422faf25f35
alif-abdullah <alifabdullah26709@gmail.com>
2017-08-21 02:11:05 +0600

    done with first page

--- src/BranchingSecond.java
+++ src/BranchingSecond.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -5,10 +6,18 @@ public class BranchingSecond {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//		initializing selenium with firefox
 		System.setProperty("webdriver.gecko.driver", "C://geckodriver.exe");
 		WebDriver driver = new FirefoxDriver ();
 		
+//		locate the site and put the credentials
 		driver.get("http://newtours.demoaut.com");
+		driver.findElement(By.xpath("html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input")).sendKeys("demo");
+		driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[3]/td[2]/input")).sendKeys("demo");
+		driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[4]/td[2]/div/input")).submit();
+		
+		
 
 	}
 
